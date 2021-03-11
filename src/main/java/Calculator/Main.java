d8ee09bb992cc6bc9491366bd1b278e74a313db0
Mohit Lakhotia <mohit24lakh@gmail.com>
2021-03-11 21:29:16 +0530

    Changes in test.java

--- src/main/java/Calculator/Main.java
+++ src/main/java/Calculator/Main.java
@@ -4,18 +4,20 @@ import java.util.*;
 import java.lang.Math;
 public class Main
 {
+    public Main() {
+    }
     public static void main(String args[])
-    {
+    {   Main m = new Main();
         int flag=0,ch;
-        double num;
+        double num,num1,num2;
         Scanner reader = new Scanner(System.in);
         System.out.println("Calculator System");
         do
         {
             System.out.println("Option Menu");
             System.out.println("");
-            System.out.println("1) Square root function");
-            System.out.println("2) Factorial function");
+            System.out.println("1) Square root");
+            System.out.println("2) Factorial");
             System.out.println("3) Natural logarithm (base е )");
             System.out.println("4) Power function");
             System.out.println("5) Exit");
@@ -33,29 +35,42 @@ public class Main
                         System.out.println("Square root function");
                         System.out.println("Enter number");
                         num = reader.nextDouble();
-                        System.out.println(Math.sqrt(num));
+                        if(num>=0){
+                            System.out.println(m.squareroot(num));
+                        }
+                        else{
+                            System.out.println("Number can not be negative");
+                        }
                         break;
                     case 2:
                         System.out.println("Factorial function");
                         System.out.println("Enter number");
                         num = reader.nextDouble();
-                        double fact = 1;
-                        for(int i=1;i<=num;i++){
-                            fact=fact*i;
+
+                        if(num>=0){
+                            System.out.println(m.factorial(num));
                         }
-                        System.out.println(Math.sqrt(fact));
+                        else{
+                            System.out.println("Number can not be negative");
+                        }
                         break;
                     case 3:
                         System.out.println("Natural logarithm (base е )");
                         System.out.println("Enter number");
                         num = reader.nextDouble();
-                        System.out.println(Math.log(num));
+                        System.out.println(m.log(num));
                         break;
                     case 4:
                         System.out.println("Power function  ");
-                        System.out.println("Enter number");
-                        num = reader.nextDouble();
-                        System.out.println(Math.pow(num,2.0));
+                        System.out.println("Enter first number");
+                        num1 = reader.nextDouble();
+                        System.out.println("Enter second number");
+                        num2 = reader.nextDouble();
+                        if(num1==0 && num2==0){
+                            System.out.println("Not defined");
+                            break;
+                        }
+                        System.out.println(m.power(num1,num2));
                         break;
                     default: System.out.println("Exiting program due to invalid input");
                         flag=1;
@@ -63,5 +78,24 @@ public class Main
             }
             System.out.println("\n");
         }while(flag==0);
+    }
+    public double squareroot(double number) {
+        double result = Math.sqrt(number);
+        return result;
+    }
+    public double factorial(double number) {
+        double fact = 1;
+        for(int i=1;i<=number;i++) {
+            fact = fact * i;
+        }
+        return fact;
+    }
+    public double log(double number) {
+        double result = Math.log(number);
+        return result;
+    }
+    public double power(double number1,double number2) {
+        double result = Math.pow(number1,number2);
+        return result;
     }
 }
--- src/test/java/Calculator/AppTest.java
+++ src/test/java/Calculator/AppTest.java
@@ -1,4 +1,59 @@
 package Calculator;
+import Calculator.Main;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
 
 public class AppTest {
+    Main m = new Main();
+    private static final double DELTA = 1e-15;
+    @Test
+    public void squareRootTruePositive(){
+        assertEquals("Square Root of an integer for True Positive", 4, m.squareroot(16),DELTA);
+        assertEquals("Square Root of an integer for True Positive", 4.0, m.squareroot(16.0),DELTA);
+    }
+
+    @Test
+    public void squareRootFalsePositive(){
+        assertNotEquals("Square Root of an integer for False Positive", 6, m.squareroot(16), DELTA);
+        assertNotEquals("Square Root of an integer for False Positive", 4.2, m.squareroot(16.0), DELTA);
+    }
+
+    @Test
+    public void factorialTruePositive(){
+        assertEquals("Factorial of an integer for True Positive", 120, m.factorial(5), DELTA);
+        assertEquals("Factorial of a double for True Positive", 24.0, m.factorial(4.0), DELTA);
+    }
+
+    @Test
+    public void factorialFalsePositive(){
+        assertNotEquals("Factorial of an integer for False Positive", 126, m.factorial(5), DELTA);
+        assertNotEquals("SFactorial of an double for False Positive", 9.0, m.factorial(4.0), DELTA);
+    }
+
+    @Test
+    public void logTruePositive(){
+        assertEquals("Logarithm of an integer for True Positive", 0, m.log(1), DELTA);
+        assertEquals("Logarithm of a double for True Positive", 0.0, m.log(1.0), DELTA);
+    }
+
+    @Test
+    public void logFalsePositive(){
+        assertNotEquals("Logarithm of an integer for False Positive", 3, m.log(1), DELTA);
+        assertNotEquals("Logarithm of an integer for False Positive", 2.1, m.log(1.0), DELTA);
+    }
+
+    @Test
+    public void powTruePositive(){
+        assertEquals("Power function on two integer numbers for True Positive", 2, m.power(2, 1), DELTA);
+        assertEquals("Power function on two double numbers for True Positive",16.0, m.power(2.0, 4.0), DELTA);
+
+    }
+
+    @Test
+    public void powFalsePositive(){
+        assertNotEquals("Power function on two integer numbers for False Positive", 1, m.power(2, 1), DELTA);
+        assertNotEquals("Power function on two double numbers for False Positive", 0, m.power(0, 0), DELTA);
+
+    }
 }
